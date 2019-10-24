e3d48b1e84492fe9fe0e8f8a953a0c11389aa430
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-10-24 16:12:25 +0000

    Alteração de exoect para not_to na string_nao_vazia

--- exemplo_rspec_tdd/spec/subject_implicito/string_spec.rb
+++ exemplo_rspec_tdd/spec/subject_implicito/string_spec.rb
@@ -2,7 +2,7 @@ require 'string_nao_vazia'
 describe String do
   describe StringNaoVazia do
     it "Não está vazia" do
-      expect(subject).to eq("Texto padrão")
+      expect(subject).not_to eq("")
     end
   end
 end
