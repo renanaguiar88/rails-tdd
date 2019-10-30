e7737c7d9a59c030c56b0eec479eef4dff25f353
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-10-30 15:40:29 +0000

    Matchers de erros

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,6 +1,16 @@
 require 'calculator'
 
 describe Calculator, "Calculadora" do
+
+  context '#div' do
+    it 'divide by 0' do      
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error("divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'only positive' do
       result = subject.sum(5, 7)
