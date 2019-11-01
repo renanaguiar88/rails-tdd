d10cf290362fbff172103531a6d8fcb9d5632235
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-01 16:09:27 +0000

    Matcher satisfy

---
+++ exemplo_rspec_tdd/spec/matchers/satisfy/satisfy_spec.rb
@@ -0,0 +1,13 @@
+describe 'satisfy' do
+  it {
+    expect(10).to satisfy('be a multiple of 2') do |x| 
+      x % 2 == 0
+    end
+  }
+  
+  it {
+    expect(9).to satisfy('be a multiple of 3') do |x| 
+      x % 3 == 0
+    end
+  }
+end
