ab06eb92f23df75a1a6706500f4362f3169522cf
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-12 15:51:52 +0000

    Fixtures

--- test_app/app/models/customer.rb
+++ test_app/app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  def full_name
+    "Mr(s) #{name}"
+  end
 end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :customers
+
+  it 'Create a customer' do
+    customer = customers(:ruby_on_rails)
+    expect(customer.full_name).to eq("Mr(s) Ruby on Rails")
+  end
 end
