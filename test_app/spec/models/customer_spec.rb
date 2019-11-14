47ce66d6c0077962856eb1133f70407955074ae1
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-14 15:43:26 +0000

    attributes_for

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -17,5 +17,11 @@ RSpec.describe Customer, type: :model do
     expect(customer.vip).to be_falsey   
   end
 
+  it '#attributes_for' do
+    attrs = attributes_for(:customer)
+    customer = Customer.create(attrs)
+    expect(customer.full_name).to start_with("Mr(s)")    
+  end
+
   it {expect{create(:customer)}.to change {Customer.all.size}.by(1)}
 end
