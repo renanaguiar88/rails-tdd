085842bbe01723d2feb08a9e8b94202f35a4db08
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-14 16:27:50 +0000

    Traits

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -24,9 +24,20 @@ RSpec.describe Customer, type: :model do
   end
 
   it 'Transient attribute' do
-    customer = create(:customer_default, upcased: true)
-    puts attributes_for(:customer_default) 
+    customer = create(:customer_default, upcased: true)    
     expect(customer.name.upcase).to eq(customer.name)
+  end
+
+  it 'Default female customer' do
+    customer = create(:customer_female_default) 
+    expect(customer.gender).to eq('F')
+    expect(customer.vip).to be_falsey
+  end
+
+  it 'VIP male customer' do
+    customer = create(:customer_male_vip) 
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to be_truthy
   end
 
   it {expect{create(:customer)}.to change {Customer.all.size}.by(1)}
