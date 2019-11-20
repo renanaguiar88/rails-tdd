4fe2e737cb446d1f81f7b3f364d47c55aaa52d8c
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-20 16:02:59 +0000

    VCR - Matching on body

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,6 +1,6 @@
 describe 'HTTParty' do
-  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do     
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do     
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1, 2, 3, 4].sample}")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)    
   end
