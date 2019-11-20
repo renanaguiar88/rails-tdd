213c6dc7b28d2a64b4e001564dd2a4517f1e039c
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-20 15:47:53 +0000

    VCR usage w/ rspec metadata

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,11 +1,7 @@
 describe 'HTTParty' do
-  it 'content-type' do
-    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-     
-    VCR.use_cassette('jsonplaceholder/posts') do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
-    end
+  it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do     
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)    
   end
 end
