95a778e5170b5f96d84140efb872782a6b54524f
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-11-28 16:05:48 +0000

    Features specs - capybara gem and screenshots configuration

---
+++ test_app/spec/features/customers_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "Customers", type: :feature, js: true do
+  it 'visit the index page' do
+    visit(customers_path)
+    page.save_screenshot('screenshot.png')
+    expect(page).to have_current_path(customers_path)
+  end
+end
