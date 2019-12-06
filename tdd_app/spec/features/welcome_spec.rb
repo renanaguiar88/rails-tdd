d0304a956e02f6b4fd1debfcd6ab6ec87231b5f4
Renan Cadore Aguiar <renan_aguiar88@yahoo.com.br>
2019-12-06 15:46:46 +0000

    Feature spec - Customer Register

--- tdd_app/spec/features/welcome_spec.rb
+++ tdd_app/spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario 'Shows the welcome message' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Welcome')
+  end
+
+  scenario 'Verify Customer Register link' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Customer Register')
   end
 end
